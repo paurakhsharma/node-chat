8c790d89686f70f3f3bff0c335292c0510a94b13
Paurakh Sharma Humagain <paurakh011@gmail.com>
2018-08-02 22:22:55 +0545

    Add jQuery and forum for message

--- public/js/index.js
+++ public/js/index.js
@@ -10,11 +10,20 @@ socket.on('disconnect', function() {
 
 socket.on('newMessage', function(message) {
     console.log(message)
+    var li = jQuery('<li></li>')
+    li.text(`${message.from}: ${message.text}`)
+
+    jQuery('#messages').append(li)
 })
 
-socket.emit('createMessage', {
-    from: "Paurakh",
-    text: "K xa keto tero"
-}, function(result) {
-    console.log(result)
+
+jQuery('#message-form').on('submit', function(e) {
+    e.preventDefault()
+    socket.emit('createMessage', {
+        from: 'User',
+        text: jQuery('[name=message]').val(),
+        createdAt: new Date().getTime()
+    }, function(result) {
+        console.log(result)
+    })
 })
