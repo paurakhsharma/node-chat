428cb5e04569d92f0a5816c2ea499be0421c8b80
Paurakh Sharma Humagain <paurakh011@gmail.com>
2018-08-30 00:11:41 +0545

    Add methods to getUsers, removeUsers, and respective tests

--- server/utils/users.js
+++ server/utils/users.js
@@ -10,13 +10,35 @@
 // getUsersList(room)
 
 class Users {
+
   constructor () {
     this.users = []
   }
+
   addUser (id, name, room) {
     let user = {id, name, room}
     this.users.push(user)
     return user
+  }
+
+  removeUser (id) {
+    var user = this.getUser(id)
+
+    if(user) {
+      this.users = this.users.filter((user) => user.id !== id)
+    }
+
+    return user
+  }
+
+  getUser(id) {
+    return this.users.filter((user) => user.id === id)[0]
+  }
+
+  getUsersList(room) {
+     let users = this.users.filter((user) => user.room === room)
+     var namesArray =  users.map((user) => user.name)
+     return namesArray
   }
 }
 
--- server/utils/users.test.js
+++ server/utils/users.test.js
@@ -3,6 +3,28 @@ const expect =  require('expect')
 const {Users} = require('./users')
 
 describe('Users', ()=> {
+  var users;
+
+  beforeEach(() => {
+    users = new Users()
+    users.users = [{
+      id: 1,
+      name: 'Nepali keto',
+      room: 'Sagarmatha'
+    },
+    {
+      id: 2,
+      name: 'Nepali thito',
+      room: 'Machhapuchre'
+    },
+    {
+      id: 3,
+      name: 'Nepali lathe',
+      room: 'Sagarmatha'
+    },
+  ]
+  })
+
   
   it('should add new user', ()=> {
     let users = new Users()
@@ -15,6 +37,39 @@ describe('Users', ()=> {
     users.addUser(user.id, user.name, user.room)
 
     expect(users.users).toEqual([user])
+  })
+  
+
+  it('should return name of users in Sagarmatha room', ()=> {
+    let usersList = users.getUsersList('Sagarmatha')
+
+    expect(usersList).toEqual(['Nepali keto', 'Nepali lathe'])
+  })
+
+  it('should return name of user of id: 2', ()=> {
+    let user = users.getUser(2);
+
+    expect(user).toEqual({'id': 2, 'name': 'Nepali thito', 'room': 'Machhapuchre'})
+  })
+
+  it('should not return name of user of id: 4', ()=> {
+    let userName = users.getUser(4);
+
+    expect(userName).toEqual(undefined)
+  })
+
+  it('should remove a user', ()=> {
+    let removedUser = users.removeUser(1)
+
+    expect(removedUser.id).toEqual(1)
+    expect(users.users.length).toEqual(2)
+  })
+
+  it('should not remove a user', ()=> {
+    let removedUser = users.removeUser(25)
+
+    expect(removedUser).toEqual(undefined)
+    expect(users.users.length).toEqual(3)
   })
 
 })
